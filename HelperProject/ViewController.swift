9186b8152175ca5929e160d550fc42ae8baaa730
YangLi <15830034@qq.com>
2016-06-04 09:13:11 +0800

    第一版，项目初始化

---
+++ HelperProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelperProject
+//
+//  Created by liyang@l2cplat.com on 16/6/4.
+//  Copyright © 2016年 yang_li828@163.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
